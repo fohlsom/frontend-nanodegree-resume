29b2f9bdaa264030e8ca88a17dc2dd9dac317421
fohlsom <ohlsson.fredrik@gmail.com>
2014-12-29 23:00:35 -0800

    Minor changes.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,32 +1,54 @@
-// var awesomeThoughts = "I am " + name + " and I am awesome.";
-// var funThoughts = awesomeThoughts.replace("awesome", "fun");
+var skills = ["python", "golf", "traveling"];
 
-// console.log(awesomeThoughts);
+var bio = {
+	"name" : "Fredrik Ohlsson",
+	"role" : "Web Developa",
+	"contact" : {
+		"email" : "ohlsson.fredrik@gmail.com",
+		"mobile" : "+1 415-425-6150",
+		"github" : "fohlsom",
+		"location" : "San Francisco" 
+	},
+	"welcomMessage" : "lorem ipsum dolor sit amet etc etc etc...",
+	"skills": [
+		"python", "golf", "traveling"
+	],
+	"bioPic" : "http://www.gravatar.com/avatar/8cb7a6c31eca402b7db2c33f7d763782.png",
+}
 
-// var email = "fohlsson@google.com";
-// var newEmail = email.replace("google", "gmail")
+var work = {
+	"position" : "Global Product Expert",
+	"tenure" : 5,
+	"city" : "Mountain View"
+}
 
-// console.log(email);
-// console.log(newEmail);
-
-// $("#main").append(awesomeThoughts);
-// $("#main").append(funThoughts);
-// $("#main").append(["Fredrik Ohlsson"]);
-
-
-// var formattedName = HTMLheaderName.replace("%data%", "Fredrik Ohlsson");
-// var formattedRole = HTMLheaderRole.replace("%data%", "Web Developer");
+var project = {
+	"name" : "Golmap.co",
+	"projectURL" : "golfmap.co",
+	"description" : "Aggregate golf tee-times on a map with reviews."
+}
 
 
-// $("#header").prepend(formattedRole);
-// $("#header").prepend(formattedName);
 
-// var str = "audacity";
-// var str2 = str.charAt(1).toUpperCase() + str.slice(2);
+// var education = {
+// 	"schools" : [
+// 		{
+// 			"name" : "Lunds University",
+// 			"attended" : "2002 - 2003",
+// 			"city" : "Lund"
+// 		},
+// 		{
+// 			"name" : "Stockholm University",
+// 			"attended" : "2001 - 2002, 2003 - 2005",
+// 			"city" : "Stockholm"
+// 		}		
+// 	]
+// }
 
-// var myArray = ["kalle", "hakan", "pollo"];
-
-// console.log(myArray.length);
+$("#main").append(bio.name);
+$("#main").append(bio.role);
+$("#main").append(work["position"]);
+// $("#main").append(education.school);
 
 
 
